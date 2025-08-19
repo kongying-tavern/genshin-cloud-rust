76c6581187d11140499ee33203795edf1bbc177a
伊欧 <langyo.china@gmail.com 1755612220 +0800>
2025-08-19 22:03:40 +0800

    🐛 Fall back to a downgraded compatible parser since serde-urlencoded mishandles tagged query params.

--- packages/router/src/routes/system/oauth.rs
+++ packages/router/src/routes/system/oauth.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
 use serde::Deserialize;
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr};
 
 use axum::{
-    extract::{ConnectInfo, Form, Json, Query},
+    extract::{ConnectInfo, Form, Json, Multipart, Query},
     http::StatusCode,
     response::IntoResponse,
 };
@@ -14,11 +14,21 @@ use _functions::functions::system::oauth::{
 };
 
 #[derive(Deserialize)]
-#[serde(rename_all = "snake_case", tag = "type")]
-pub enum LoginParams {
-    Password { username: String, password: String },
-    ClientCredentials { scope: String },
-    RefreshToken { refresh_token: String },
+pub struct LoginQuery {
+    grant_type: Option<LoginQueryType>,
+    // ClientCredentials
+    scope: Option<String>,
+    // RefreshToken
+    refresh_token: Option<String>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "snake_case", untagged)]
+pub enum LoginQueryType {
+    // 这里留着只是用于标记，Password 这个项是故意忽略的，因为这个类型只能手动解析
+    // Password,
+    ClientCredentials,
+    RefreshToken,
 }
 
 #[tracing::instrument(skip_all)]
@@ -26,38 +36,85 @@ pub async fn oauth(
     ConnectInfo(native_ip): ConnectInfo<SocketAddr>,
     ExtractIP(ip): ExtractIP,
     ExtractUserAgent(user_agent): ExtractUserAgent,
-    Query(query): Query<Option<LoginParams>>,
-    Form(form): Form<Option<LoginParams>>,
+    Query(query): Query<LoginQuery>,
+    mut form: Multipart,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
     let ip = ip.unwrap_or(native_ip);
 
-    if let Some(form) = form {
-        match form {
-            LoginParams::Password { username, password } => Ok(Json(
-                oauth_password_login(username, password)
-                    .await
-                    .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?,
+    let mut form_fields = {
+        let mut ret = HashMap::new();
+        while let Some(field) = form.next_field().await.map_err(|err| {
+            (
+                StatusCode::BAD_REQUEST,
+                format!("Failed to read form field: {}", err),
             )
-            .into_response()),
-            _ => return Err((StatusCode::BAD_REQUEST, "Invalid grant type".into())),
+        })? {
+            let name = field
+                .name()
+                .ok_or((StatusCode::BAD_REQUEST, "Field name is required".into()))?
+                .to_string();
+            let value = field.text().await.map_err(|err| {
+                (
+                    StatusCode::BAD_REQUEST,
+                    format!("Failed to read form field {}: {}", name, err),
+                )
+            })?;
+            ret.insert(name, value);
         }
-    } else if let Some(query) = query {
-        match query {
-            LoginParams::ClientCredentials { scope } => Ok(Json(
+        ret
+    };
+    if !form_fields.is_empty() {
+        let grant_type = form_fields
+            .remove("grant_type")
+            .ok_or((StatusCode::BAD_REQUEST, "Grant type is required".into()))?;
+        if grant_type != "password" {
+            return Err((StatusCode::BAD_REQUEST, "Invalid grant type".into()));
+        }
+
+        let username = form_fields
+            .remove("username")
+            .ok_or((StatusCode::BAD_REQUEST, "Username is required".into()))?;
+        let password = form_fields
+            .remove("password")
+            .ok_or((StatusCode::BAD_REQUEST, "Password is required".into()))?;
+        return Ok(Json(
+            oauth_password_login(username, password)
+                .await
+                .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?,
+        )
+        .into_response());
+    }
+
+    match query
+        .grant_type
+        .ok_or((StatusCode::BAD_REQUEST, "Grant type is required".into()))?
+    {
+        LoginQueryType::ClientCredentials => {
+            let scope = query.scope.ok_or_else(|| {
+                (
+                    StatusCode::BAD_REQUEST,
+                    "Scope is required for client credentials".into(),
+                )
+            })?;
+            return Ok(Json(
                 oauth_client_credentials(scope)
                     .await
                     .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?,
             )
-            .into_response()),
-            LoginParams::RefreshToken { refresh_token } => {
-                oauth_refresh(refresh_token)
-                    .await
-                    .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
-                Ok(().into_response())
-            }
-            _ => return Err((StatusCode::BAD_REQUEST, "Invalid grant type".into())),
+            .into_response());
         }
-    } else {
-        return Err((StatusCode::BAD_REQUEST, "No parameters provided".into()));
+        LoginQueryType::RefreshToken => {
+            let refresh_token = query.refresh_token.ok_or_else(|| {
+                (
+                    StatusCode::BAD_REQUEST,
+                    "Refresh token is required for refresh token grant type".into(),
+                )
+            })?;
+            oauth_refresh(refresh_token)
+                .await
+                .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
+            return Ok(StatusCode::NO_CONTENT.into_response());
+        }
+        _ => return Err((StatusCode::BAD_REQUEST, "Invalid grant type".into())),
     }
 }
