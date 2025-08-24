7a8e50281f9f26660fef43c2574f9daa4cbe6fdc
伊欧 <langyo.china@gmail.com 1756055969 +0800>
2025-08-25 01:19:29 +0800

    🐛 Fix a route template that AI autocomplete got wrong.

--- packages/router/src/routes/api/area/mod.rs
+++ packages/router/src/routes/api/area/mod.rs
@@ -14,10 +14,10 @@ use axum::{
 pub async fn router() -> Result<Router> {
     let ret = Router::new()
         .route("/get/list", post(list::list))
-        .route("/get/:area_id", post(get::get))
+        .route("/get/{area_id}", post(get::get))
         .route("/add", put(add::add))
         .route("/update", post(update::update))
-        .route("/:area_id", delete(delete::delete));
+        .route("/{area_id}", delete(delete::delete));
 
     Ok(ret)
 }
